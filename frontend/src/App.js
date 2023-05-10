1b2f5a875af6dd65a874c46ba70832b218835b61
Olga Chernega <eliang.cheng@gmail.com>
2023-05-10 12:18:52 +0300

    feat: add front-end authentication and context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import AuthPage from "./pages/Auth";
+import AuthPage from "./pages/AuthPage/AuthPage";
 import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
 import Navigation from "./components/Navigation/Navigation";
+import AuthContext from "./context/authContext";
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+
+  const login = (token, userId, tokenExpiration) => {
+    setToken(token);
+    setUserId(userId);
+  };
+
+  const logout = () => {
+    setToken(null);
+    setUserId(null);
+  };
+
   return (
     <BrowserRouter>
-      <Navigation />
-      <main className="main-content">
-        <Routes>
-          <Route
-            path="/"
-            element={<Navigate to="/auth" />}
-          />
-          <Route
-            path="/auth"
-            element={<AuthPage />}
-          />
-          <Route
-            path="/events"
-            element={<EventsPage />}
-          />
-          <Route
-            path="/bookings"
-            element={<BookingsPage />}
-          />
-        </Routes>
-      </main>
+      <AuthContext.Provider
+        value={{
+          token: token,
+          userId: userId,
+          login: login,
+          logout: logout
+        }}>
+        <Navigation />
+        <main className="main-content">
+          <Routes>
+            {!token && (
+              <Route
+                path="/"
+                element={<Navigate to="/auth" />}
+              />
+            )}
+            {token && (
+              <Route
+                path="/"
+                element={<Navigate to="/events" />}
+              />
+            )}
+            {token && (
+              <Route
+                path="/auth"
+                element={<Navigate to="/events" />}
+              />
+            )}
+
+            {!token && (
+              <Route
+                path="/auth"
+                element={<AuthPage />}
+              />
+            )}
+            <Route
+              path="/events"
+              element={<EventsPage />}
+            />
+            {token && (
+              <Route
+                path="/bookings"
+                element={<BookingsPage />}
+              />
+            )}
+          </Routes>
+        </main>
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import authContext from "../../context/authContext";
 
 import "./Navigation.css";
 
-const Navigation = props => (
-  <header className="main-navigation">
-    <div className="main-navigation__logo">
-      <h1>Evently</h1>
-    </div>
-    <nav className="main-navigation__items">
-      <ul>
-        <li>
-          <NavLink to="/auth">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/events">Events</NavLink>
-        </li>
-        <li>
-          <NavLink to="/bookings">Bookings</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const Navigation = props => {
+  const authCtx = useContext(authContext);
+
+  return (
+    <header className="main-navigation">
+      <div className="main-navigation__logo">
+        <h1>Evently</h1>
+      </div>
+      <nav className="main-navigation__items">
+        <ul>
+          {!authCtx.token && (
+            <li>
+              <NavLink to="/auth">Login</NavLink>
+            </li>
+          )}
+          <li>
+            <NavLink to="/events">Events</NavLink>
+          </li>
+          {authCtx.token && (
+            <li>
+              <NavLink to="/bookings">Bookings</NavLink>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navigation;
